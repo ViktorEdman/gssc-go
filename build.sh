eba7fc655ad94069e43cb0321dcdfc9e34d74dbe
Viktor Edman <viktor@edman.se>
2024-04-26 13:09:21 +0200

    sync

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-pnpm build && atlas schema apply --auto-approve --to "file://schema.sql" --url "sqlite://data.db" --dev-url "sqlite://dev?mode=memory" && sqlc generate && templ generate && go build -o ./bin/gssc .
+pnpm build && atlas schema apply --auto-approve --to "file://schema.sql" --url "sqlite://data.db" --dev-url "sqlite://dev?mode=memory" && sqlc generate && templ generate && go build -o ./bin/gssc-go-$(date "+%d-%m-%y") .
+rsync -avh /home/viktor/dev/go/gssc-go/bin/ /mnt/nvme/file_browser/srv/gssc-go/
