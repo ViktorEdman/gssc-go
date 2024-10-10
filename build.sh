e826403db3798bb6de20e90c80cfd4f6fe52a740
Viktor Edman <viktor@edman.se>
2024-10-10 09:27:35 +0200

    fixed htmx depency

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 pnpm build && atlas schema apply --auto-approve --to "file://schema.sql" --url "sqlite://data.db" --dev-url "sqlite://dev?mode=memory" && sqlc generate && templ generate && go build -o ./bin/gssc-go-$(date "+%d-%m-%y") .
+go build -o ./gssc-go
 rsync -avh /home/viktor/dev/go/gssc-go/bin/ /mnt/nvme/file_browser/srv/gssc-go/
