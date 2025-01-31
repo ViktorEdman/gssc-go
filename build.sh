a8bc2abd4bd02eaf0785991aaaf236fa9612c089
Viktor Edman <viktor@edman.se>
2025-01-31 08:46:27 +0100

    all statuses performance optimization

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 pnpm build && atlas schema apply --auto-approve --to "file://schema.sql" --url "sqlite://data.db" --dev-url "sqlite://dev?mode=memory" && sqlc generate && templ generate && go build -o ./bin/gssc-go-$(date "+%d-%m-%y") .
 go build -o ./gssc-go
+cp ./schema.sql ./bin/schema-$(date "+%d-%m-%y").sql
 rsync -avh /home/viktor/dev/go/gssc-go/bin/ /mnt/nvme/file_browser/srv/gssc-go/
